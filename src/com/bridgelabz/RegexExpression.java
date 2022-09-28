c2f62cfbcbe8d84d710712d7249162a71c507f78
pavithramuddu93 <pavithramuddu93>
2022-09-28 20:57:52 +0530

    Added UC2: last name

--- src/com/bridgelabz/RegexExpression.java
+++ src/com/bridgelabz/RegexExpression.java
@@ -3,6 +3,8 @@
  */
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
 /**
  * @author PAVITHRA C
  *
@@ -12,9 +14,26 @@ public class RegexExpression {
 	/**
 	 * @param args
 	 */
+	static boolean validate(String pattern, String text) {
+		return Pattern.compile(pattern).matcher(text).matches();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to Regex");
-	}
+			String pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
 
-}
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Please Enter Last name:");
+
+			String lastName = scanner.next();
+
+			boolean isValid = validate(pattern, lastName);
+
+			if (isValid) {
+				System.out.println("This is valid last name .");
+			} else {
+				System.out.println("Not a valid name");
+			}
+		}
+
+	}
