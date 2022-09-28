aadba31c5ec40f6e8c1346eebef5a489d76be530
pavithramuddu93 <pavithramuddu93>
2022-09-28 20:39:11 +0530

    Welcome message added

---
+++ src/com/bridgelabz/RegexExpression.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author PAVITHRA C
+ *
+ */
+public class RegexExpression {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to Regex");
+	}
+
+}
