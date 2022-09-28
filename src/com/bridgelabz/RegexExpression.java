0039248b5d9ee800e73869e717f1f79ca9a2e845
pavithramuddu93 <pavithramuddu93>
2022-09-28 20:50:08 +0530

    Added UC1: valid first name

--- src/com/bridgelabz/RegexExpression.java
+++ src/com/bridgelabz/RegexExpression.java
@@ -3,6 +3,9 @@
  */
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author PAVITHRA C
  *
@@ -12,9 +15,30 @@ public class RegexExpression {
 	/**
 	 * @param args
 	 */
+	public static boolean checkValidPincode(String firstName) {
+		String firstNameRegex= "^[A-Z]{1}[A-Za-z]{2,}$";
+		Pattern pattrenObject= Pattern.compile(firstNameRegex);
+		
+		if(firstName==null) {
+			return false;
+			
+		}
+		Matcher matcherObject = pattrenObject.matcher(firstName);
+        return matcherObject.matches();
+	} 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to Regex");
+System.out.println("----- Welcome To Pin Code Validatior -----");
+		
+		String firstName ="Pavi";
+		boolean isfirstName = checkValidPincode(firstName);
+		
+		if(isfirstName)
+			System.out.println(firstName+" is an Valid firstName");
+		else
+			System.out.println(firstName+" is an Invalid firstName");
 	}
 
 }
+	
+
